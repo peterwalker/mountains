932e132fc48ff1298112e26dfd46d57d91668654
peterwalker <peterbwalker@me.com>
2013-03-28 09:21:23 -0400

    first cut

---
+++ buildSrc/src/main/groovy/org/gradleware/ConvertMountainXml.groovy
@@ -0,0 +1,30 @@
+package org.gradleware
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+
+class ConvertMountainXml extends DefaultTask {
+	@InputFile
+	File xmlFile
+
+	@Input
+	String separator
+
+	@OutputDirectory
+	File outputDir
+
+	ConvertMountainXml() {
+		outputs.upToDateWhen {
+
+		}
+	}
+
+	@TaskAction
+	public void generate() {
+		def mountains = new XmlSlurper().parse(xmlFile)
+		mountains.mountain.each { mountain ->
+			def mountainFile = new File(outputDir, mountain.name.text() + ".txt")
+			mountainFile.text = "feet$separator${mountain.height.text()}"
+		}
+	}
+}
