245f1db5cdb33a2914975c14bfc7bdd1cf9c8f5e
peterwalker <peterbwalker@me.com>
2013-04-02 17:19:38 -0400

    incremental

--- buildSrc/src/main/groovy/org/gradleware/ConvertAllMountainsXml.groovy
+++ buildSrc/src/main/groovy/org/gradleware/ConvertAllMountainsXml.groovy
@@ -2,6 +2,8 @@ package org.gradleware
 
 import org.gradle.api.*
 import org.gradle.api.tasks.*
+import org.gradle.api.execution.*
+import org.apache.commons.io.*
 
 class ConvertAllMountainsXml extends DefaultTask {
 	@InputDirectory
@@ -17,16 +19,20 @@ class ConvertAllMountainsXml extends DefaultTask {
 		description = "Converts all mountains files"
 		group = "Mountain"
 	}
-	
+
 	@TaskAction
-	public void generate() {
-		inputDir.eachFile{ file->
-			def mountains = new XmlSlurper().parse(file)
-			mountains.mountain.each { mountain ->
-				def mountainFile = new File(outputDir, mountain.name.text() + ".txt")
-				mountainFile.text = "feet$separator${mountain.height.text()}"
-			}
+	public void generate(TaskInputChanges inputs) {
+		inputs.outOfDate({ change ->
+			logger.lifecycle ("$change.file is out of date")
+			inputDir.eachFile { file-> processFile(change.file) }
+		} as Action)
+	}
+
+	def processFile(file){
+		def mountains = new XmlSlurper().parse(file)
+		mountains.mountain.each { mountain ->
+			def mountainFile = new File(outputDir, mountain.name.text() + ".txt")
+			mountainFile.text = "feet$separator${mountain.height.text()}"
 		}
 	}
 }
-
